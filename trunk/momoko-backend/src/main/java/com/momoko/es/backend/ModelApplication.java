5a42c802352aa41b0f8deba92de184bb86111e75
ramon-jose.casares <ramon-jose.casares@ESD2C63852.itc.com>
2017-08-17 15:16:13 +0200

    Añadido el sistema de autenticacion.

--- trunk/momoko-backend/src/main/java/com/momoko/es/backend/ModelApplication.java
+++ trunk/momoko-backend/src/main/java/com/momoko/es/backend/ModelApplication.java
@@ -6,16 +6,155 @@
  */
 package com.momoko.es.backend;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.Filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.CompositeFilter;
+
+import com.momoko.es.backend.autenticacion.ClientResources;
 
 @SpringBootApplication
-@EnableOAuth2Sso
-public class ModelApplication {
+@EnableOAuth2Client
+@RestController
+@EnableAuthorizationServer
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class ModelApplication extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    OAuth2ClientContext oauth2ClientContext;
+    
     public static void main(final String[] args) {
         SpringApplication.run(ModelApplication.class, args);
     }
 
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+      return principal;
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**").authorizeRequests()
+            .antMatchers("/", "/login**", "/webjars/**").permitAll()
+            .anyRequest().authenticated()                          
+        .and().exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
+        .and().logout().logoutSuccessUrl("/").permitAll()
+        .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+    }
+    
+    @RequestMapping("/unauthenticated")
+    public String unauthenticated() {
+      return "redirect:/?error=true";
+    }
+    
+    @Bean
+//    public AuthoritiesExtractor authoritiesExtractor(OAuth2RestOperations template) {
+//      return map -> {
+//        String url = (String) map.get("organizations_url");
+//        @SuppressWarnings("unchecked")
+//        List<Map<String, Object>> orgs = template.getForObject(url, List.class);
+//        if (orgs.stream()
+//            .anyMatch(org -> "spring-projects".equals(org.get("login")))) {
+//          return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
+//        }
+//        throw new BadCredentialsException("Not in Spring Projects origanization");
+//      };
+//    }
+    
+    /**
+     * Sso filter.
+     *
+     * @return the filter
+     */
+    private Filter ssoFilter() {       
+        CompositeFilter filter = new CompositeFilter();
+        List<Filter> filters = new ArrayList<>();
+        filters.add(ssoFilter(facebook(), "/login/facebook"));
+        filters.add(ssoFilter(gmail(), "/login/gmail"));        
+        return filter;
+      }
+    
+    /**
+     * Sso filter.
+     *
+     * @param client client
+     * @param path path
+     * @return the filter
+     */
+    private Filter ssoFilter(ClientResources client, String path) {
+        OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);
+        OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
+        filter.setRestTemplate(template);
+        UserInfoTokenServices tokenServices = new UserInfoTokenServices(
+            client.getResource().getUserInfoUri(), client.getClient().getClientId());
+        tokenServices.setRestTemplate(template);
+        filter.setTokenServices(tokenServices);
+        return filter;
+      }
+    
+    @Bean
+    @ConfigurationProperties("gmail")
+    public ClientResources gmail() {
+      return new ClientResources();
+    }
+
+    @Bean
+    @ConfigurationProperties("facebook")
+    public ClientResources facebook() {
+      return new ClientResources();
+    }
+    
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(
+        OAuth2ClientContextFilter filter) {
+      FilterRegistrationBean registration = new FilterRegistrationBean();
+      registration.setFilter(filter);
+      registration.setOrder(-100);
+      return registration;
+    }
+    
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+      @Override
+      public void configure(HttpSecurity http) throws Exception {
+        http
+          .antMatcher("/me")
+          .authorizeRequests().anyRequest().authenticated();
+      }
+    }
 }
--- trunk/momoko-backend/src/test/java/com/momoko/es/model/ModelApplicationTests.java
+++ trunk/momoko-backend/src/test/java/com/momoko/es/model/ModelApplicationTests.java
@@ -6,12 +6,16 @@
  */
 package com.momoko.es.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ModelApplicationTests {
 
@@ -19,4 +23,36 @@ public class ModelApplicationTests {
     public void contextLoads() {
     }
 
+    
+    @Test
+    public void connectJDBCToAWSEC2() {
+
+        System.out.println("----MySQL JDBC Connection Testing -------");
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your MySQL JDBC Driver?");
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println("MySQL JDBC Driver Registered!");
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.
+                    getConnection("jdbc:mysql://ec2-52-37-81-48.us-west-2.compute.amazonaws.com:3306/momokobd", 
+                            "momokobdUser", "momokoBDpass");
+        } catch (SQLException e) {
+            System.out.println("Connection Failed!:\n" + e.getMessage());
+        }
+
+        if (connection != null) {
+            System.out.println("SUCCESS!!!! You made it, take control     your database now!");
+        } else {
+            System.out.println("FAILURE! Failed to make connection!");
+        }
+
+    }
 }
