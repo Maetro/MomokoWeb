d48d0f7396ac43bb7628abcb60ea690e884080cc
DESKTOP-D2RKKSB\RMaet <RMaetro@gmail.com>
2018-05-17 01:37:55 +0200

    Fix

--- momoko-frontend/src/main/momoko/src/app/contenido/libro/ficha-libro/ficha-libro.component.ts
+++ momoko-frontend/src/main/momoko/src/app/contenido/libro/ficha-libro/ficha-libro.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  Inject,
+  PLATFORM_ID
+} from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { Libro } from '../../../dtos/libro';
 import { LibroSimple } from '../../../dtos/libroSimple';
@@ -14,12 +20,13 @@ import { LibroService } from '../../../services/libro.service';
   styleUrls: ['./ficha-libro.component.css']
 })
 export class FichaLibroComponent implements OnInit {
-
   private log = environment.log;
 
   anchura: number;
 
   url: string;
+
+  public href: string = '';
 
   suscriptor: any;
 
@@ -36,33 +43,36 @@ export class FichaLibroComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private titleService: Title,
-    private metaService: Meta, 
-    @Inject(PLATFORM_ID) private platformId: Object) {
-
-    }
+    private metaService: Meta,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   ngOnInit() {
     if (this.log) {
       console.log('Creando pagina de la entrada');
     }
-    const columna = document.getElementById('mirarAnchura')
+    const columna = document.getElementById('mirarAnchura');
     const width = columna.offsetWidth;
     const style = window.getComputedStyle(columna);
     // tslint:disable-next-line:radix
     const margin = parseInt(style.paddingLeft) + parseInt(style.paddingRight);
     this.anchura = width - margin;
+    this.href = this.router.url;
     this.route.data.subscribe((data: { fichaLibro: FichaLibro }) => {
       this.libro = data.fichaLibro.libro;
       this.librosParecidos = data.fichaLibro.cincoLibrosParecidos;
       this.tresUltimasEntradas = data.fichaLibro.tresUltimasEntradas;
       let autores = '';
       this.libro.autores.forEach(autor => {
-        autores = autores + autor.nombre + ', '
+        autores = autores + autor.nombre + ', ';
       });
       autores = autores.substring(0, autores.length - 2);
       const titulo = 'Ficha de libro ' + this.libro.titulo;
-      const metatituloPagina = 'Encuentra aquí toda la información sobre ' + this.libro.titulo +
-        ' y sobre ' + autores;
+      const metatituloPagina =
+        'Encuentra aquí toda la información sobre ' +
+        this.libro.titulo +
+        ' y sobre ' +
+        autores;
       this.titleService.setTitle(titulo);
       // Changing meta with name="description"
       const tag = { name: 'description', content: metatituloPagina };
@@ -74,19 +84,29 @@ export class FichaLibroComponent implements OnInit {
       this.metaService.removeTag('name="og:title"');
       this.metaService.removeTag('name="og:description"');
       this.metaService.removeTag('name="og:image"');
-      this.metaService.addTag({ name: 'og:url', content: 'https://momoko.es/libro/'+ this.libro.urlLibro });
+      this.metaService.addTag({
+        name: 'og:url',
+        content: 'https://momoko.es/libro/' + this.libro.urlLibro
+      });
       this.metaService.addTag({ name: 'og:locale', content: 'es_ES' });
-      this.metaService.addTag({ name: 'fb:app_id', content: '1932678757049258' });
+      this.metaService.addTag({
+        name: 'fb:app_id',
+        content: '1932678757049258'
+      });
       this.metaService.addTag({ name: 'og:type', content: 'book' });
       this.metaService.addTag({ name: 'og:title', content: this.libro.titulo });
-      this.metaService.addTag({ name: 'og:description', content: this.libro.resumen});
-      this.metaService.addTag({ name: 'og:image', content: this.libro.urlImagen });
+      this.metaService.addTag({
+        name: 'og:description',
+        content: this.libro.resumen
+      });
+      this.metaService.addTag({
+        name: 'og:image',
+        content: this.libro.urlImagen
+      });
     });
-
   }
 
   mirarAnchura(): number {
     return this.anchura;
   }
 }
-
